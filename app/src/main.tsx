2dbb816a8d883543c2c5aae62789f8b8c0271acb
Lovisa Ivarsson <lovisaivarsson@LIN43.localdomain>
2024-01-26 18:41:43 +0100

    added routes and simple layout

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './pages/layout.tsx';
+import Home from './pages/home.tsx';
+import NoPage from './pages/NoPage.tsx';
+import Game from './pages/Game.tsx';
+// import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="game" element={<Game />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
---
+++ app/src/pages/Game.tsx
@@ -0,0 +1,5 @@
+const Game = () => {
+    return <h1>Contact Me</h1>;
+  };
+  
+  export default Game;
