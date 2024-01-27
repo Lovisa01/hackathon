4754ed7aa9af1cdd4c4496ad2f1aef958afba509
joalu827 <joalu827@student.liu.se>
2024-01-27 17:55:54 +0100

    pretty done

--- app/src/pages/Game.tsx
+++ app/src/pages/Game.tsx
@@ -3,10 +3,7 @@ import Board from '../components/gameComponents/Board';
 
 const Game = () => {
     return (
-        <div className="game">
-            <h1>4-In-A-Row Game</h1>
-            <Board />
-        </div>
+        <Board/>
     );
 };
 
